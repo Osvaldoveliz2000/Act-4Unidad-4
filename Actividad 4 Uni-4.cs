bdd9ff4b92b434f3f633aaefd47c8935c7a58e72
Osvaldoveliz2000 <125836784+Osvaldoveliz2000@users.noreply.github.com>
2023-03-14 00:03:31 -0600

    Add files via upload

---
+++ Actividad 4 Uni-4.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+// Definición de la interfaz
+public interface IAnimal
+{
+    void hacerSonido();
+}
+
+// Definición de la clase que implementa la interfaz
+public class Perro : IAnimal
+{
+    public void hacerSonido()
+    {
+        Console.WriteLine("El perro está ladrando.");
+    }
+}
+
+// Uso de la interfaz para implementar el polimorfismo
+public class Program
+{
+    static void Main(string[] args)
+    {
+        IAnimal miAnimal;
+
+        miAnimal = new Perro();
+        miAnimal.hacerSonido();
+    }
+}
